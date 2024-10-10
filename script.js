cf14aa8d0d14968a07540ddb56eae8fcc8ba84e4
rosu <mentitas@tuta.io>
2024-10-09 23:02:19 -0300

    pls centrate

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ window.onload = (event) => {
 function changeColor() {
 
     letter = alphabet[Math.floor(Math.random() * 16)];
-    number = 1 + Math.floor(Math.random() * 30);
+    number = Math.floor(Math.random() * 30);
 
-    formatted_number = ("0" + number).slice(-2);
+    formatted_number = ("0" + (number+1)).slice(-2);
 
     document.getElementById('background').style.backgroundColor = colors[letter][number]
     document.getElementById('label_color').textContent = letter + "-" + formatted_number;
